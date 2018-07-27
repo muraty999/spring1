af838b297b3b836938f4c6b20aafbeee9dc9224c
Murat YILMAZ <muraty999@gmail.com>
2018-07-27 19:59:20 +0300

    Autowired

--- src/main/java/com/luv2code/springdemo/TrackCoach.java
+++ src/main/java/com/luv2code/springdemo/TrackCoach.java
@@ -1,9 +1,16 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class TrackCoach implements Coach {
 
+	@Autowired
+	Fortune fortune;
+	
 	public String getDailyWork() {
-		return "Run a 5 day";
+		return "Run a 5 minute" +"\n" + this.fortune.getMessage();
 	};
 
 }
---
+++ src/main/java/com/luv2code/springdemo/newway/WithSpringComponents.java
@@ -0,0 +1,26 @@
+
+package com.luv2code.springdemo.newway;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.luv2code.springdemo.Coach;
+
+public class WithSpringComponents {
+
+	public static void main(String[] args)  {
+		
+		// load the spring configuration file
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+				
+		// retrieve bean from spring container
+		Coach theCoach = context.getBean("trackCoach", Coach.class);
+		
+		// call methods on the bean
+		System.out.println(theCoach.getDailyWork());
+		
+		// close the context
+		context.close();
+	}
+
+}
