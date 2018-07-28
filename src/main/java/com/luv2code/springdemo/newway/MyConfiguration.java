797b8fd2074f03c5671a27302d0c941a0ea136ac
Murat YILMAZ <muraty999@gmail.com>
2018-07-28 15:33:38 +0300

    Properties file

--- src/main/java/com/luv2code/springdemo/newway/MyConfiguration.java
+++ src/main/java/com/luv2code/springdemo/newway/MyConfiguration.java
@@ -2,9 +2,11 @@ package com.luv2code.springdemo.newway;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan("com.luv2code.springdemo")
+@PropertySource("classpath:application.propterties")
 public class MyConfiguration {
 
 }
